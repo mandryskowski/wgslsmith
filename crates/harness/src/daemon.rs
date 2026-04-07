29e47655ef53268fc8fa23c02051fd5f655b3c1e
mandryskowski <nedesf@gmail.com>
2026-04-07 15:57:29 +0100

    Print out executed shader seed in daemon

--- crates/harness/src/daemon.rs
+++ crates/harness/src/daemon.rs
@@ -175,6 +175,9 @@ impl DaemonServer {
                 )?;
                 writer.flush()?;
 
+                // Print out the seed
+                println!("{}", req.execution_input.shader.split_once('\n').unwrap().0);
+
                 let state = Arc::new(AtomicU8::new(0)); // 0: running, 1: finished, 2: timed out
                 let state_clone = state.clone();
 
