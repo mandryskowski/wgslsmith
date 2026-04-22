05f1cc82c1018465552f78b8c523e928ac6d2abb
mandryskowski <nedesf@gmail.com>
2026-04-22 13:44:58 +0100

    .

--- build.py
+++ build.py
@@ -158,23 +158,21 @@ def cargo_build(package, target=None, cwd=None, features=[]):
                 wrapper_path.write_text("#!/bin/bash\nargs=()\nfor arg in \"$@\"; do\n  if [ \"$arg\" != \"-nodefaultlibs\" ]; then\n    args+=(\"$arg\")\n  fi\ndone\nexec clang++ \"${args[@]}\"\n")
                 wrapper_path.chmod(0o755)
 
-            asan_options_path = Path(cwd if cwd else ".").absolute().joinpath("asan_options.c")
-            if not asan_options_path.exists():
-                asan_options_path.write_text("""
-const char* __asan_default_options() { return "detect_odr_violation=0:detect_leaks=0:symbolize=0"; }
-const char* __ubsan_default_options() { return "print_stacktrace=0"; }
-""")
+            dummy_c_path = Path(cwd if cwd else ".").absolute().joinpath("asan_dummy.c")
+            if not dummy_c_path.exists():
+                dummy_c_path.write_text("void __asan_dummy() {}\n")
             
-            asan_options_obj = Path(cwd if cwd else ".").absolute().joinpath("asan_options.o")
+            dummy_obj = Path(cwd if cwd else ".").absolute().joinpath("asan_dummy.o")
             if args.asan or args.ubsan:
-                subprocess.run(["clang", "-c", str(asan_options_path), "-o", str(asan_options_obj)]).check_returncode()
+                subprocess.run(["clang", *san_flags, "-c", str(dummy_c_path), "-o", str(dummy_obj)]).check_returncode()
 
             rustflags = env.get("RUSTFLAGS", "")
             rustflags += f" -C linker={wrapper_path}"
+            if args.asan or args.ubsan:
+                rustflags += f" -C link-arg={dummy_obj}"
             if args.asan:
                 rustflags += " -C link-arg=-fsanitize=address"
                 rustflags += " -C link-arg=-Wl,-rpath,@executable_path"
-                rustflags += f" -C link-arg={asan_options_obj}"
             if args.ubsan:
                 rustflags += " -C link-arg=-fsanitize=undefined"
             rustflags += " -C link-arg=-lc++"
