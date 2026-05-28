48bf9ec8b750e2ad05acfce56e886c7b4011b035
Michal Andryskowski <nedesf@gmail.com>
2026-05-28 19:50:04 +0100

    Fix smoothstep concretization

--- crates/concretizer/src/concretizer.rs
+++ crates/concretizer/src/concretizer.rs
@@ -678,7 +678,7 @@ impl Concretizer {
             }
 
             if ident == "smoothstep" {
-                if let (Some(Some(low)), Some(Some(high))) = (vals.get(1), vals.get(2)) {
+                if let (Some(Some(low)), Some(Some(high))) = (vals.get(0), vals.get(1)) {
                     if helper::is_invalid_smoothstep_bounds(low, high) {
                         return self.default_node(data_type);
                     }
