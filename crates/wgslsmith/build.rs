cb6954e822f3bd22577613b10ba30e19033d177b
mandryskowski <nedesf@gmail.com>
2026-07-14 08:38:41 +0200

    Print commit dates in wgslsmith info

--- crates/wgslsmith/build.rs
+++ crates/wgslsmith/build.rs
@@ -2,9 +2,9 @@ use std::env;
 use std::fs;
 use std::process::Command;
 
-fn get_git_hash(dir: &str) -> Option<String> {
+fn get_git_info(dir: &str, commit: &str) -> Option<String> {
     let output = Command::new("git")
-        .args(["rev-parse", "HEAD"])
+        .args(["log", "-1", "--format=%H (%cd)", "--date=short", commit])
         .current_dir(dir)
         .output()
         .ok()?;
@@ -31,23 +31,29 @@ fn is_dir_dirty(dir: &str, path: &str) -> bool {
 fn main() {
     println!("cargo:rerun-if-env-changed=DAWN_BUILD_DIR");
 
-    let mut wgslsmith_hash = get_git_hash("../..").unwrap_or_else(|| "unknown".to_string());
+    let mut wgslsmith_hash = get_git_info("../..", "HEAD").unwrap_or_else(|| "unknown".to_string());
     if is_dir_dirty("../..", "crates") {
         wgslsmith_hash.push_str(" (dirty)");
     }
 
     let dawn_hash = if let Ok(dawn_build_dir) = env::var("DAWN_BUILD_DIR") {
         let commit_path = std::path::Path::new(&dawn_build_dir).join("COMMIT");
-        if let Ok(commit) = fs::read_to_string(commit_path) {
-            format!("{} (pre-built)", commit.trim())
+        if let Ok(commit_content) = fs::read_to_string(commit_path) {
+            let commit = commit_content.trim();
+            if let Some(info) = get_git_info("../../external/dawn", commit) {
+                format!("{} (pre-built)", info)
+            } else {
+                format!("{} (pre-built)", commit)
+            }
         } else {
             "unknown (pre-built)".to_string()
         }
     } else {
-        get_git_hash("../../external/dawn").unwrap_or_else(|| "unknown".to_string())
+        get_git_info("../../external/dawn", "HEAD").unwrap_or_else(|| "unknown".to_string())
     };
 
-    let wgpu_hash = get_git_hash("../../external/wgpu").unwrap_or_else(|| "unknown".to_string());
+    let wgpu_hash =
+        get_git_info("../../external/wgpu", "HEAD").unwrap_or_else(|| "unknown".to_string());
 
     println!("cargo:rustc-env=WGSLSMITH_GIT_HASH={}", wgslsmith_hash);
     println!("cargo:rustc-env=DAWN_GIT_HASH={}", dawn_hash);
